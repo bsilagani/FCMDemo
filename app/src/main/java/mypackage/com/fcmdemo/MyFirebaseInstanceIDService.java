efeaf3b910b65e81bc17973f222fbd8cf8a8aec5
bsilagani <sbharath2006@gmail.com>
2017-11-08 23:43:23 +0530

    Initial Commit

---
+++ app/src/main/java/mypackage/com/fcmdemo/MyFirebaseInstanceIDService.java
@@ -0,0 +1,27 @@
+package mypackage.com.fcmdemo;
+
+/**
+ * Created by bharathsilagani on 08/11/17.
+ */
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+
+    @Override
+    public void onTokenRefresh() {
+
+        //For registration of token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        //To displaying token on logcat
+        Log.d("TOKEN: ", refreshedToken);
+
+    }
+
+}
